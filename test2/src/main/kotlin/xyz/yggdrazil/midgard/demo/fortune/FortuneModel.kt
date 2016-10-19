8817bfd852b65d503f929c66e8ec308984ffe313
Alexandre Mommers <alexandre.a.mommers@bnpparibas-pf.com>
2016-10-19 17:57:55 +0200

    update fortune test model

--- test2/src/main/kotlin/xyz/yggdrazil/midgard/demo/fortune/FortuneModel.kt
+++ test2/src/main/kotlin/xyz/yggdrazil/midgard/demo/fortune/FortuneModel.kt
@@ -4,5 +4,9 @@ package xyz.yggdrazil.midgard.demo.fortune
  * Created by Alexandre Mommers on 18/10/16.
  */
 class FortuneModel {
-    data class Settings()
+    data class Settings(var sites: Int = 10000,
+                        var loydRelxations: Int = 2,
+                        var seed: Long = System.nanoTime())
+
+    val settings = Settings()
 }
